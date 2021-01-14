9c07d8579738496c02fd3463e63a04c8657e6e66
haliltokszz <toksozhalil@gmail.com>
2021-01-14 18:23:06 +0300

    Created Generic Repository Created ORM (EntityFramework)

---
+++ Rent-a-Car.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Rent_a_Car.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Rent_a_Car.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class, IEntity, new()
+    {
+        T Get(Expression<Func<T,bool>> filter=null);
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Rent-a-Car.Core/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rent_a_Car.Core.Entities
+{
+    public interface IEntity
+    {
+    }
+}
