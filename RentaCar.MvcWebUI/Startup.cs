6cb04f39f58a04e759a8906020d2dbd474401467
haliltokszz <toksozhalil@gmail.com>
2021-01-24 04:16:22 +0300

    Scoped and Singleton services added to Startup.cs

--- RentaCar.MvcWebUI/Startup.cs
+++ RentaCar.MvcWebUI/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using RentaCar.Business.Abstract;
+using RentaCar.Business.Concrete;
+using RentaCar.DataAccess.Abstract;
+using RentaCar.DataAccess.Concrete;
 
 namespace RentaCar.MvcWebUI
 {
@@ -15,6 +19,14 @@ namespace RentaCar.MvcWebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IUnitofWork,UnitofWork>();
+            //services.AddScoped<IUserService, UserManager>();
+            services.AddScoped<ICustomerService, CustomerManager>();
+            services.AddScoped<IEmployeeService, EmployeeManager>();
+            services.AddScoped<ICarService, CarManager>();
+            services.AddScoped<ICompanyService, CompanyManager>();
+            services.AddScoped<IRentalService, RentalManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
