51ebfecac98407ff2551ce6ed9208c72a77c03b0
haliltokszz <toksozhalil@gmail.com>
2021-01-27 16:52:37 +0300

    Authentication Manager is completed

---
+++ RentaCar.Business/Abstract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RentaCar.Entities.Abstract;
+using RentaCar.Entities.Dtos;
+
+namespace RentaCar.Business.Abstract
+{
+    public interface IAuthService
+    {
+        string Login(UserForLoginDto userForLoginDto, string userType);
+        Users CreatePasswordHash(Users user, string password);
+        bool UserExist(string userName, string userType);
+    }
+}
