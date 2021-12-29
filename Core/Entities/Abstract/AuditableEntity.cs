c5aa3caaf5f9ac1e87745611f5d76c59f27b0450
haliltokszz <toksozhalil@gmail.com>
2021-12-30 01:47:44 +0300

    feature: AuditableEntity.cs added

---
+++ Core/Entities/Abstract/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.Entities.Abstract
+{
+    public abstract class AuditableEntity : IEntity
+    {
+        public string Id { get; set; } = new Guid().ToString();
+        public DateTime CreatedTime { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool Deleted { get; set; } = false;
+        public DateTime DeletedTime { get; set; }
+    }
+}
