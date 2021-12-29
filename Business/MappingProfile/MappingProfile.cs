65e5ce4d6a286100f8d744958a22e4303c7dec43
haliltokszz <toksozhalil@gmail.com>
2021-12-30 02:28:22 +0300

    feature: automapper added

---
+++ Business/MappingProfile/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.MappingProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDetailDto>().ReverseMap();
+            CreateMap<User, UserForRegisterDto>().ReverseMap();
+        }
+    }
+}
